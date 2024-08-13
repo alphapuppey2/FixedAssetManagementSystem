16bd617f771b3ad483a259d2a32e06b056a9d0de
lourdsam <17100411@usc.edu.ph>
2024-08-14 01:53:44 +0800

    Sidebar UI for worker/user

--- project-app/resources/views/user/home.blade.php
+++ project-app/resources/views/user/home.blade.php
@@ -1,13 +1,4 @@
 <!-- resources/views/user/home.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>User Home</title>
-</head>
-<body>
-    <h1>Welcome, User!</h1>
-    <p>This is the user landing page.</p>
-</body>
-</html>
+@include('layouts.sideBarUser')
+
+
