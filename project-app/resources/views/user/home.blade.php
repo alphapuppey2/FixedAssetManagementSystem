03332e8df1dd6492b1fc13aacdc48e572030d359
lourdsam <17100411@usc.edu.ph>
2024-08-16 23:22:46 +0800

    routes for sidebar, temp ui for scan qr and request list

--- project-app/resources/views/user/home.blade.php
+++ project-app/resources/views/user/home.blade.php
@@ -1,4 +1,11 @@
 <!-- resources/views/user/home.blade.php -->
 @include('layouts.sideBarUser')
 
+<div class="ml-64 p-8">
 
+    @yield('scanQR-content')
+    @yield('requestList-content')
+    @yield('notification-content')
+    @yield('profile-content')
+
+</div>
