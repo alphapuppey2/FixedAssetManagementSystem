fa01828d9b9ae4cc4a3120afc44cf3aa4360a1f2
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2024-10-17 18:21:14 +0800

    Assigned By Feature is Goods na Maintenance History Search Filtering Changing not Rendering Auto.

--- project-app/app/Models/User.php
+++ project-app/app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      */
 
      protected $fillable = [
+        'id',
         'firstname',
         'middlename',
         'lastname',
--- project-app/resources/views/dept_head/partials/generalInfo.blade.php
+++ project-app/resources/views/dept_head/partials/generalInfo.blade.php
@@ -24,7 +24,8 @@
             <div class="info flex items-center p-4 bg-white">
                 <label class="field-label mr-4 w-32 text-gray-600 font-semibold text-base">Name:</label>
                 <div class="field-Info font-semibold view-only text-base">{{ $data->name }}</div>
-                <x-text-input name="name" class="edit hidden w-full border-gray-300 text-base" value="{{ $data->name }}" />
+                <x-text-input name="name" class="edit hidden w-full border-gray-300 text-base"
+                    value="{{ $data->name }}" />
             </div>
 
             {{-- Category --}}
@@ -33,9 +34,9 @@
                 <div class="field-Info font-semibold view-only text-base">{{ $data->category }}</div>
                 <select name="category" class="edit hidden w-full border-gray-300 text-base">
                     @foreach ($categories['ctglist'] as $category)
-                    <option value="{{ $category->id }}" @selected($data->category == $category->name)>
-                        {{ $category->name }}
-                    </option>
+                        <option value="{{ $category->id }}" @selected($data->category == $category->name)>
+                            {{ $category->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -46,9 +47,9 @@
                 <div class="field-Info font-semibold view-only text-base">{{ $data->model }}</div>
                 <select name="mod" class="edit hidden w-full border-gray-300 text-base">
                     @foreach ($model['mod'] as $model)
-                    <option value="{{ $model->id }}" @selected($data->model == $model->name)>
-                        {{ $model->name }}
-                    </option>
+                        <option value="{{ $model->id }}" @selected($data->model == $model->name)>
+                            {{ $model->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -59,9 +60,9 @@
                 <div class="field-Info font-semibold view-only text-base">{{ $data->manufacturer }}</div>
                 <select name="mcft" class="edit hidden w-full border-gray-300 text-base">
                     @foreach ($manufacturer['mcft'] as $manufacturer)
-                    <option value="{{ $manufacturer->id }}" @selected($data->manufacturer == $manufacturer->name)>
-                        {{ $manufacturer->name }}
-                    </option>
+                        <option value="{{ $manufacturer->id }}" @selected($data->manufacturer == $manufacturer->name)>
+                            {{ $manufacturer->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -72,9 +73,9 @@
                 <div class="field-Info font-semibold view-only text-base">{{ $data->location }}</div>
                 <select name="loc" class="edit hidden w-full border-gray-300 text-base">
                     @foreach ($location['locs'] as $location)
-                    <option value="{{ $location->id }}" @selected($data->location == $location->name)>
-                        {{ $location->name }}
-                    </option>
+                        <option value="{{ $location->id }}" @selected($data->location == $location->name)>
+                            {{ $location->name }}
+                        </option>
                     @endforeach
                 </select>
             </div>
@@ -85,19 +86,29 @@
                 <div class="field-Info font-semibold view-only text-base">
                     @include('components.asset-status', ['status' => $data->status])
                 </div>
-                <select name="status" class="edit hidden w-full border-gray-300 text-base">
+                <select name="status" id="status" onchange="toggleRequired(this)"
+                    class="edit hidden w-full border-gray-300 text-base">
                     @foreach ($status['sts'] as $stat)
-                    <option value="{{ $stat }}" @selected($data->status == $stat)>{{ $stat === "under_maintenance" ? 'under maintenance' : $stat }}</option>
+                        <option value="{{ $stat }}" @selected($data->status == $stat)>
+                            {{ $stat === 'under_maintenance' ? 'under maintenance' : $stat }}
+                        </option>
                     @endforeach
                 </select>
             </div>
 
             {{-- Last Used By --}}
             <div class="info flex items-center p-4 bg-white">
-                <label class="field-label mr-4 w-32 text-gray-600 font-semibold text-base">Last Used By:</label>
-                <div class="field-Info font-semibold view-only text-base">{{ $data->lastname . ', ' . $data->firstname }}</div>
-                <select name="usrAct" class="edit hidden w-full border-gray-300 text-base">
-                    <option value="">Select a user</option>
+                <label class="field-label mr-4 w-32 text-gray-600 font-semibold text-base">Assigned to:</label>
+                <div class="field-Info font-semibold view-only text-base">
+                    {{ isset($data->lastname) ? $data->lastname . ', ' . $data->firstname : 'N/A' }}</div>
+                <select name="usrAct" id="selectUsers" class="edit hidden w-full border-gray-300 text-base">
+                    @foreach ($allUserInDept as $itemOption)
+                        <option value="{{ $itemOption->id }}" @selected($data->lastname == $itemOption->lastname)>
+                            {{ $itemOption->firstname . ' ' . $itemOption->lastname }}
+                        </option>
+                    @endforeach
+                    <option value ='' @selected(!isset($data->lastname))>Assign no one</option>
+
                 </select>
             </div>
         </div>
@@ -108,11 +119,9 @@
             {{-- Asset Image --}}
             <div class="imgContainer flex flex-col items-center space-y-4">
                 <label class="font-semibold text-gray-700">Asset Image</label>
-                <div class="imageField w-40 h-40 relative flex justify-center items-center border-2 border-gray-200 rounded-lg shadow-md overflow-hidden">
-                    <img
-                        src="{{ asset($imagePath) }}"
-                        id="imagePreview"
-                        alt="Asset Image"
+                <div
+                    class="imageField w-40 h-40 relative flex justify-center items-center border-2 border-gray-200 rounded-lg shadow-md overflow-hidden">
+                    <img src="{{ asset($imagePath) }}" id="imagePreview" alt="Asset Image"
                         class="w-full h-full object-cover">
                 </div>
 
@@ -125,14 +134,16 @@
             {{-- QR Code --}}
             <div class="qrContainer flex flex-col items-center space-y-4">
                 <label class="font-semibold text-gray-700">QR Code</label>
-                @if($data->qr_img)
-                <a href="{{ asset('storage/' . $data->qr_img) }}" download="{{ $data->code }}" class="block w-40 h-40">
-                    <img src="{{ asset('storage/' . $data->qr_img) }}" alt="QR Code" class="w-full h-full object-contain">
-                </a>
+                @if ($data->qr_img)
+                    <a href="{{ asset('storage/' . $data->qr_img) }}" download="{{ $data->code }}"
+                        class="block w-40 h-40">
+                        <img src="{{ asset('storage/' . $data->qr_img) }}" alt="QR Code"
+                            class="w-full h-full object-contain">
+                    </a>
                 @else
-                <div class="QRBOX w-40 h-40 border-2 border-gray-200 rounded-lg shadow-md">
-                    <img src="{{ asset($qrCodePath) }}" alt="QR Code" class="w-full h-full object-contain">
-                </div>
+                    <div class="QRBOX w-40 h-40 border-2 border-gray-200 rounded-lg shadow-md">
+                        <img src="{{ asset($qrCodePath) }}" alt="QR Code" class="w-full h-full object-contain">
+                    </div>
                 @endif
             </div>
         </div>
@@ -142,20 +153,52 @@
 </form>
 
 @if (session('success'))
-<div id="toast" class="fixed bottom-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow">
-    {{ session('success') }}
-</div>
+    <div id="toast" class="fixed bottom-4 right-4 bg-green-500 text-white px-4 py-2 rounded shadow">
+        {{ session('success') }}
+    </div>
 @endif
 
 @vite(['resources/js/flashNotification.js'])
 
 <script>
+    function toggleRequired(select) {
+        const selectUsersInput = document.getElementById('selectUsers');
+        const statusAssetInput = document.getElementById('status');
+        if (select.value === 'deployed') {
+            selectUsersInput.setAttribute('required', 'required'); // Set required
+        } else {
+            selectUsersInput.removeAttribute('required');
+            selectUsersInput.value = '';
+             // Remove required
+        }
+    }
+
     document.addEventListener('DOMContentLoaded', function() {
         const editButton = document.getElementById('editBTN');
         const saveButton = document.getElementById('saveBTN');
         const cancelButton = document.getElementById('cancelBTN');
         const editElements = document.querySelectorAll('.edit');
         const viewElements = document.querySelectorAll('.view-only');
+
+        // assigned to and also status to interactions
+        const selectUsersInput = document.getElementById('selectUsers');
+        const statusAssetInput = document.getElementById('status');
+
+        let initialUsrActValue = selectUsersInput.value;
+        let initialStatusValue = statusAssetInput.value;
+
+
+        selectUsersInput.addEventListener('change', function() {
+            if (initialUsrActValue === '' && selectUsers.value !== '') {
+                // Change the status to 'deployed' if usrAct changes from empty to non-empty
+                statusAssetInput.value = 'deployed';
+            }
+            else{
+                statusAssetInput.value = initialStatusValue;
+            }
+        });
+
+        // -------------------
 
         editButton.addEventListener('click', () => {
             editElements.forEach(el => el.classList.remove('hidden'));
