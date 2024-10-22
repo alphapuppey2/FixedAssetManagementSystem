6fa007f48cb12b9ae6bab2f6b934e0f43ee05189
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2024-10-22 12:42:06 +0800

    configuring session timeout

--- project-app/config/session.php
+++ project-app/config/session.php
@@ -32,9 +32,9 @@ return [
     |
     */
 
-    'lifetime' => env('SESSION_LIFETIME', 1),
+    'lifetime' => env('SESSION_LIFETIME', 120),
 
-    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
+    'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', true),
 
     /*
     |--------------------------------------------------------------------------
