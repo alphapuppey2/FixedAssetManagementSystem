0ce5ba93866688706975192f5a3a5b4faf8f9429
lourdsam <17100411@usc.edu.ph>
2024-11-03 17:57:02 +0800

    proper time in system ok, in .env
    APP_TIMEZONE=Asia/Manila

--- project-app/config/database.php
+++ project-app/config/database.php
@@ -56,6 +56,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET time_zone = "+08:00"', // Set the timezone to Philippine Time (UTC+8)
             ]) : [],
         ],
 
