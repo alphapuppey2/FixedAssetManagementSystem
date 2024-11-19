4cda8c93684ad03626363b6c505108e9716be697
Joshua Loui Soqueno <80311305+alphapuppey2@users.noreply.github.com>
2024-11-20 01:31:37 +0800

    setting page in dept head
    removing redundant error handling

--- project-app/resources/views/dept_head/setting.blade.php
+++ project-app/resources/views/dept_head/setting.blade.php
@@ -17,15 +17,6 @@
         @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
-                    @foreach ($errors->all() as $err)
-                        <li>{{ $err }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
                     @foreach ($errors->all() as $error)
                         <li>{{ $error }}</li>
                     @endforeach
