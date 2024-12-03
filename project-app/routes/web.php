ccc9dec31820ae87571d9cb4cd9ce3183358ce5a
lourdsam <17100411@usc.edu.ph>
2024-12-03 14:34:05 +0800

    removed technical from system

--- project-app/routes/web.php
+++ project-app/routes/web.php
@@ -223,7 +223,6 @@ Route::middleware(['adminUserType', 'auth', 'verified'])->group(function () {
 
 // DeptHead Routes
 Route::middleware(['deptHeadUserType', 'auth', 'verified'])->group(function () {
-    Route::post('/save-new-manufacturers', [AsstController::class, 'saveNewManufacturers'])->name('save.new.manufacturers');
 
     /*
     -------------------
